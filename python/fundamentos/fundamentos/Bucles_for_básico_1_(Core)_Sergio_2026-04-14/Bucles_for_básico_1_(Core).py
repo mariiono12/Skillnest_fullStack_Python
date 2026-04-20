0352792150e9444aa4fd0d3022aa4475d0599812
SokA4 <bustosian432@gmail.com>
2026-04-20 11:06:46 -0400

    updte

--- python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Sergio_2026-04-14/Bucles_for_básico_1_(Core).py
+++ python/fundamentos/fundamentos/Bucles_for_básico_1_(Core)_Sergio_2026-04-14/Bucles_for_básico_1_(Core).py
@@ -24,29 +24,72 @@ def potenciadoresEnergia():
 # (Tu código aquí)
 def trampaEmojis():
  for i in range (1, 101):
-   if i % 5 == "0":
-     print(f"(i) 😂")
-   elif i % 10 == "0":
-     print(f"(i) 😂") 
-
+     if i % 5 == 0:
+      print("😒")
+     elif i % 10 == 0:
+      print("😂") 
+     else:
+      print(i)
 # 4. Suma colosal
 # Suma todos los números pares del 0 al 500,000 e imprime la suma total.
 # (Tu código aquí)
-
-
+def sumaColosal():
+    total = 0
+    for i in range(0, 500001, 2):
+        total =+ i
+    print(f"La suma total es: {total}")
 # 5. Retroceso temporal
 # Desde 2024, retrocede de 3 en 3 hasta 0 o menos.
 # Imprime cada valor en la cuenta regresiva.
 # (Tu código aquí)
-
+def retrocesoTemporal():
+ for i in range(2024, -2, -3):
+   print(i)
 
 # 6. Contador dinámico
 # Declara las variables inicio, fin, y salto (por ejemplo: inicio=3, fin=10, salto=2).
 # Imprime los números en el rango que sean múltiplos de 'salto'.
 # (Tu código aquí)
-
+def contadorDinamico():
+ inicio = 3
+ fin = 10
+ salto = 2
+ for i in range (inicio, fin + 1):
+   if i % salto == 0:
+    print(i)
 # Ejemplo: si inicio = 3, fin = 10, y salto = 2
 # Se imprimiría: 4, 6, 8, 10
 
 #Menu de navegación para ejercicios
+continuar = True
+while continuar:
+    print("---Ejercicios Python---")
+    print("---1.-Ejercicio 1---")
+    print("---2.-Ejercicio 2---")
+    print("---3.-Ejercicio 3---")
+    print("---4.-Ejercicio 4---")
+    print("---5.-Ejercicio 5---")
+    print("---6.-Ejercicio 6---")
+  
+
+    opcion = input("---Elige una opción: (1-15) (0 para salir) =")
+
+    if opcion =="1":  
+        print("Ejecutando ejercicio 1: ")
+        print(generadorNiveles())
+    elif opcion == "2":
+        print("Ejecutando ejercicio 2: ")
+        print(potenciadoresEnergia())
+    elif opcion == "3":
+        print("Ejecutando ejercicio 3: ")
+        print(trampaEmojis())
+    elif opcion == "4":
+        print("Ejecutando ejercicio 4: ")
+        print(sumaColosal())
+    elif opcion == "5":
+        print("Ejecutando ejercicio 5: ")
+        print(retrocesoTemporal())
+    elif opcion == "6":
+        print("Ejecutando ejercicio 6: ")
+        print(contadorDinamico())
 
