53fc55db22e2e43d0d7da5b2e0665531cec85a5d
SokA4 <bustosian432@gmail.com>
2026-04-23 09:25:08 -0400

    update

---
+++ python/Core/Funciones_intermedias_2_(Core)_Sergio_Bustos_2026-04-23/funciones_intermedias_(Core).py
@@ -0,0 +1,24 @@
+# Ranking de puntajes de un torneo de eSports
+puntajes = [ [1000, 1500, 2000], [300, 700, 1400] ]
+puntajes = [1][0] = 600
+print(puntajes)
+# Lista de creadores de contenido en una plataforma de streaming
+
+streamers = [
+   {"nombre": "GameNinjaPro", "seguidores": 250000},
+   {"nombre": "PixelWarrior", "seguidores": 180000}
+
+]
+
+
+# Eventos en distintas ciudades del mundo
+eventos = {
+   "Estados Unidos": ["Los Ángeles", "Nueva York", "Las Vegas"],
+   "España": ["Madrid", "Barcelona", "Valencia"]
+}
+
+
+# Coordenadas de la sede de un torneo internacional
+ubicacion = [
+   {"latitud": 34.052235, "longitud": -118.243683}
+]
