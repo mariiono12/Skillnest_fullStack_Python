a6fa865655776d9062803fb6bbd79c6a10b21bf5
SokA4 <bustosian432@gmail.com>
2026-05-11 12:53:41 -0400

    update

--- python/Core/Suscripción a plataforma de streaming(Core)_Sergio_2026_05_07/suscripcion_streaming.py
+++ python/Core/Suscripción a plataforma de streaming(Core)_Sergio_2026_05_07/suscripcion_streaming.py
@@ -1,47 +1,94 @@
+import os
+def limpiar_consola():
+    os.system('cls')
+
 class SuscripcionStreaming:
-   costos_suscripcion = {"Gratis": 0, "Estándar": 5.99, "Premium": 10.99}
-   
-   def __init__(self, usuario, costo_mensual, saldo_pendiente ,tipo_suscripcion="Gratis",):
-       self.usuario = usuario
-       self.saldo_pendiente = saldo_pendiente
-       self.tipo_suscripcion = tipo_suscripcion
-       self.costo_mensual = costo_mensual
-       pass
+    costos_suscripcion = {"Gratis": 0, "Estándar": 5000, "Premium": 10000}
+    
+    # metodo constructor
+    def __init__(self, usuario, saldo_pendiente, tipo_suscripcion="Gratis", puede_ver = True):
+        self.usuario = usuario
+        self.saldo_pendiente = saldo_pendiente
+        self.tipo_suscripcion = tipo_suscripcion
+        self.puede_ver = puede_ver
+        self.costo_mensual = self.costos_suscripcion[tipo_suscripcion] # <-- Importante
 
-   def realizar_pago(self, monto):
-       if monto < 0:
-          print("Monto invalido")
-       elif monto > self.saldo_pendiente:
-           print("Monto excede al saldo pendiente")
-       else:
-           self.saldo_pendiente = monto
-           print(f"Pago realizado. Saldo pendiente: {self.saldo_pendiente}")
-       """Reduce el saldo pendiente según el monto pagado."""
-       pass
+    def realizar_pago(self, monto):
+        if self.tipo_suscripcion == "Gratis":
+                print(f"Que quieres pagar si tu suscripcion es gratis?!")
+                print(f"Tu sueldo: ${monto} fue recuperado...")
+        
+        if self.saldo_pendiente == 0 or monto >= self.costo_mensual:
+            print(f"Muchas gracias por mantenerte en nuestra plataforma!!")
+            self.puede_ver = True
+        elif monto < self.costo_mensual:
+            print(f"Pagaste: ${monto} pero debes de pagar: ${self.costo_mensual} minimo...")
+            self.puede_ver = False
+            self.saldo_pendiente -= monto
+    
+    def cambiar_suscripcion(self, nuevo_tipo):
+        self.tipo_suscripcion = nuevo_tipo
+        self.costo_mensual = self.costos_suscripcion[nuevo_tipo]
 
-   def cambiar_suscripcion(self, nuevo_tipo):
-       if nuevo_tipo in self.costos_suscripcion:
-           self.tipo_suscripcion = nuevo_tipo
-           self.saldo_pendiente = self.costos_suscripcion[nuevo_tipo]
-           print(f"Suscripción cambiada a {nuevo_tipo}")
-       else:
-        print("Tipo de suscripción inválido")
-       """Cambia el tipo de suscripción y actualiza el costo mensual."""
-       pass
+    def ver_contenido_exclusivo(self):
+        if self.tipo_suscripcion != "Gratis" and self.puede_ver:
+            print("Por favor disfruta de nuestro contenido exclusivo como tu quieras!!")
+        elif not self.puede_ver:
+            print(f"Lo sentimos pero debes de pagar tu suscripcion...")
+        elif self.tipo_suscripcion == "Gratis":
+            print(f"Lo sentimos tu suscripcion es de tipo {self.tipo_suscripcion}")
 
-   def ver_contenido_exclusivo(self):
-        if self.tipo_suscripcion in ["Estándar", "Premium"]:
-         print("Contenido exclusivo disponible")
-        else:
-         print("No tienes acceso a contenido exclusivo")
-"""Permite ver contenido exclusivo según el tipo de suscripción."""
-pass
+    def mostrar_info_suscripcion(self):
+        print(f"\nuser ⧽ {self.usuario}")
+        print(f"tipo de suscripcion ⧽ {self.tipo_suscripcion}")
+        print(f"costo mensual ⧽ ${self.costo_mensual}")
+        print(f"saldo pendiente ⧽ ${self.saldo_pendiente}")
 
-def mostrar_info_suscripcion(self):
-         print(f"Usuario: {self.usuario}")
-         print(f"Tipo de suscripción: {self.tipo_suscripcion}")
-         print(f"Costo mensual: {self.costos_suscripcion[self.tipo_suscripcion]}")
-         print(f"Saldo pendiente: {self.saldo_pendiente}")
-"""Muestra la información de la suscripción del usuario."""
-pass  
+def ejercicio_1():
+    inp = int(input("Por favor ingresar monto a pagar:\n"))
+    if inp != "":
+        usuarios[user - 1].realizar_pago(inp)
 
+def ejercicio_2():
+    sus = input("Por favor ingresar nueva suscripción:\n")
+    if sus != "":
+        usuarios[user - 1].cambiar_suscripcion(sus)
+
+
+user1 = SuscripcionStreaming("Isidora", 27000, "Estándar")
+user2 = SuscripcionStreaming("Martin", 0,)
+user3 = SuscripcionStreaming("Sergio", 300000, "Premium")
+
+usuarios = [user1, user2, user3]
+
+print("Hello again, what you wanna do today?!")
+sueño = True
+
+
+user = int(input("Podrias ingresar tu llave de usuario por favor?(1-3):_"))
+while sueño:
+    print("1-. Pagar")
+    print("2-. Cambiar suscripción")
+    print("3-. Contenido exclusivo")
+    print("4-. Info")
+    print("0-. Salir")
+    
+    hacer = int(input("Por favor ingresar actividad:_"))
+    if hacer == 1:
+        limpiar_consola()
+        ejercicio_1()
+    elif hacer == 2:
+        limpiar_consola()
+        ejercicio_2()
+    elif hacer == 3:
+        limpiar_consola()
+        usuarios[user - 1].ver_contenido_exclusivo()
+    elif hacer == 4:
+        limpiar_consola()
+        usuarios[user - 1].mostrar_info_suscripcion()
+    elif hacer == 0:
+        limpiar_consola()
+        print("^._.^ฅ")
+        sueño = False
+    else:
+        print("Opción no válida")
