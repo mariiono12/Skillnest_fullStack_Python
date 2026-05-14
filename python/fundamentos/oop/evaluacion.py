e07b0d59e16a7d80cdc2f8ec039dbff30435b6c4
SokA4 <bustosian432@gmail.com>
2026-05-14 10:55:14 -0400

    iga

---
+++ python/fundamentos/oop/evaluacion.py
@@ -0,0 +1,41 @@
+class CafeteriaCliente:
+
+   def __init__(self, nombre,puntos_acumulados = 0,saldo_pendiente = 0,tipo_de_membresía = "Bronce"):
+       self.puntos_acumulados = puntos_acumulados
+       self.nombre = nombre
+       self.saldo_pendiente = saldo_pendiente
+       self.tipo_de_membresía = tipo_de_membresía
+       # TODO:
+       # Crear los atributos faltantes
+
+       def realizar_compra(self, monto):
+
+   # TODO:
+   # Aumentar saldo pendiente
+
+   
+   # TODO:
+   # Aumentar puntos
+
+        print(f"{self.nombre} realizó una compra.")
+
+        def pagar_saldo(self, monto):
+
+   # TODO:
+   # Validar el pago
+
+   
+   @classmethod()
+   def mostrar_total_clientes(cls):
+
+   # TODO
+
+   @staticmethod
+def validar_membresia(tipo):
+
+   # TODO
+
+
+
+
+
